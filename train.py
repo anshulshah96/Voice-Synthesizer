91c38c60d41b149becd17bef5ec92ce71bb208bc
Tirth Patel <tirth.tp.97@gmail.com>
2017-03-31 01:45:36 +0530

    Delete train.py
    no use

--- train.py
+++
@@ -1,8 +0,0 @@
-from sklearn import datasets
-from sklearn.multiclass import OneVsRestClassifier
-from sklearn.svm import LinearSVC
-
-X, y = features, labels
-clf = OneVsRestClassifier(LinearSVC(C=100.))
-model = clf.fit(X, y)
-output = model.predict(X)
