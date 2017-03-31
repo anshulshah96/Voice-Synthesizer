86594d3c994dccf509dd2c321af0d64f7aa45d6f
tirth14 <tirth.tp.97@gmail.com>
2017-03-31 20:32:19 +0530

    SVM_plot added

--- SVM_classifier_final.py
+++ SVM_classifier_final.py
@@ -13,16 +13,28 @@ from sklearn import datasets
 from sklearn.externals import joblib
 
 le = LabelEncoder()
-df = pd.read_hdf("data_extract/features_dataset_2b.h5")
-NUM_PEOPLE = 5
-df = df.loc[df['id']<NUM_PEOPLE]
+df_data = pd.read_hdf("data_extract/features_dataset_2b.h5")
 clist = ["chroma{}".format(i) for i in range(12)]
 lclist = ["lc{}".format(i) for i in range(12)]
-df[lclist] = np.log(df[clist])
+df_data[lclist] = np.log(df_data[clist])
 flist = ["mfcc{}".format(i) for i in range(14)]+lclist+["centroid","crest","flatness","kurtosis","mean"]
-df_train, df_cross, df_test = get_partitions(df)
-obj = SVM(flist)
-obj.fit(df_train)
-joblib.dump(obj.model, 'saved_models/SVM5.pkl') 
-clf = joblib.load('saved_models/SVM5.pkl') 
-obj.predict_df(df_cross, clf)
+
+plot = dict()
+
+for num_per in [5,10,15,20,25,30,35,40]:
+	for train_per in [0.05,0.1,0.2,0.3,0.4]:
+		df = df_data.loc[df_data['id']<num_per]
+		df_train, df_cross, df_test = get_partitions_2(df,train_per=train_per,cross_per=(0.9-train_per),test_per=0.1)
+		obj = SVM(flist)
+		obj.fit(df_train)
+		#joblib.dump(obj, 'saved_models/SVM5.pkl') 
+		#obj2 = joblib.load('saved_models/SVM5.pkl') 
+		output = obj.predict_df(df_test)
+		x = float(filter(None, output.split('\n')[-2].split(' '))[-2])
+		print num_per,train_per,x
+		plot[(num_per,train_per)] = x
+
+print plot
+joblib.dump(plot, 'outputs/SVM_plot.pkl')
+obj2 = joblib.load('outputs/SVM_plot.pkl')
+print obj2 
