37816e53adc887100188f4b7973478d84f8d0587
tirth14 <tirth.tp.97@gmail.com>
2017-04-01 20:36:26 +0530

    Random selection of speakers

--- SVM_classifier_final.py
+++ SVM_classifier_final.py
@@ -11,6 +11,7 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn import preprocessing
 from sklearn import datasets
 from sklearn.externals import joblib
+import random
 
 le = LabelEncoder()
 df_data = pd.read_hdf("data_extract/features_dataset_2b.h5")
@@ -21,23 +22,26 @@ flist = ["mfcc{}".format(i) for i in range(14)]+lclist+["centroid","crest","flat
 
 plot = dict()
 
-for num_per in [5,10,15,20,25,30,35,40]:
-    for train_per in [0.02,0.05,0.1,0.2,0.3,0.4]:
-        df = df_data.loc[df_data['id']<num_per]
-        df_train, df_cross, df_test = get_partitions_2(df,train_per=train_per,cross_per=(0.9-train_per),test_per=0.1)
-        # obj = SVM(flist)
-        # obj.fit(df_train)
-        # #joblib.dump(obj, 'saved_models/SVM5.pkl') 
-        # #obj2 = joblib.load('saved_models/SVM5.pkl') 
-        # output = obj.predict_df(df_test)
-        obj = MultiGauss(flist)
-        obj.fit(df_train)
-        output = obj.predict_df(df_test)
-        x = float(filter(None, output.split('\n')[-2].split(' '))[-2])
-        print num_per,train_per,x
-        plot[(num_per,train_per)] = x
+for num_per in [5,10,15,20,25,30,35]:
+	ids = [i for i in range(40)]
+	for i in [5,31,35,39]:
+	    ids.remove(i)
+	random.shuffle(ids)
+	sel = [ids[i] for i in range(len(ids)) if i<num_per]
+	bool_arr = np.array([(row in sel) for row in df_data['id']])
+	df = df_data[bool_arr]
+	for train_per in [0.02,0.05,0.1,0.2,0.3,0.4]:
+		df_train, df_cross, df_test = get_partitions_2(df,train_per=train_per,cross_per=(0.9-train_per),test_per=0.1)
+		obj = SVM(flist)
+		obj.fit(df_train)
+		#obj = MultiGauss(flist)
+		#obj.fit(df_train)
+		output = obj.predict_df(df_test)
+		x = float(filter(None, output.split('\n')[-2].split(' '))[-2])
+		print num_per,train_per,x
+		plot[(num_per,train_per)] = x
 
 print plot
-joblib.dump(plot, 'outputs/MGauss_plot.pkl')
-obj2 = joblib.load('outputs/MGauss_plot.pkl')
-print obj2 
+joblib.dump(plot, 'outputs/SVM_plot2.pkl')
+#obj2 = joblib.load('outputs/MGauss_plot.pkl')
+#print obj2 
